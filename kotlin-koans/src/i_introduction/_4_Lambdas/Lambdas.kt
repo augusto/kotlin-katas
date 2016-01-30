b828a8506bcad378595ea8c7f7c5d4da6e5b469a
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2016-01-30 19:37:22 +0000

    up to i-7

--- kotlin-koans/src/i_introduction/_4_Lambdas/Lambdas.kt
+++ kotlin-koans/src/i_introduction/_4_Lambdas/Lambdas.kt
@@ -11,16 +11,16 @@ fun example() {
 }
 
 fun todoTask4(collection: Collection<Int>): Nothing = TODO(
-    """
+        """
         Task 4.
         Rewrite 'JavaCode4.task4()' in Kotlin using lambdas.
         You can find the appropriate function to call on 'collection' through IntelliJ's code completion feature.
         (Don't use the class 'Iterables').
     """,
-    documentation = doc4(),
-    references = { JavaCode4().task4(collection) })
+        documentation = doc4(),
+        references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean = collection.any { x -> x % 42 == 0 }
 
 
 
--- kotlin-koans/src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ kotlin-koans/src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -1,6 +1,7 @@
 package i_introduction._7_Nullable_Types
 
-import util.*
+import util.TODO
+import util.doc7
 
 fun test() {
     val s: String = "this variable cannot store null references"
@@ -12,23 +13,26 @@ fun test() {
 }
 
 fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO(
-    """
+        """
         Task 7.
         Rewrite JavaCode7.sendMessageToClient in Kotlin, using only one 'if' expression.
         Declarations of Client, PersonalInfo and Mailer are given below.
     """,
-    documentation = doc7(),
-    references = { JavaCode7().sendMessageToClient(client, message, mailer) }
+        documentation = doc7(),
+        references = { JavaCode7().sendMessageToClient(client, message, mailer) }
 )
 
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    val email = client?.personalInfo?.email
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
+    }
 }
 
-class Client (val personalInfo: PersonalInfo?)
-class PersonalInfo (val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
 
 interface Mailer {
     fun sendMessage(email: String, message: String)
