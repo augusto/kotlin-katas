0fd185b0d688535d645a3f02569ea0940752170b
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2016-01-31 21:40:46 +0000

    part iii conventions - 25-28. note to self: do not code after a long day when you're falling asleep

--- kotlin-koans/src/iii_conventions/MyDate.kt
+++ kotlin-koans/src/iii_conventions/MyDate.kt
@@ -1,8 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val thisValue = ((year * 100) + month) * 100 + dayOfMonth
+        val otherValue = ((other.year * 100) + other.month) * 100 + other.dayOfMonth
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+        return thisValue.compareTo(otherValue)
+    }
+}
+
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +18,26 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var current = start
+            override fun next(): MyDate {
+                if(!hasNext()) {
+                    throw IllegalStateException("no more!")
+                }
+                val tmp = current
+                current = current.nextDay()
+                return tmp;
+            }
+
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+        }
+    }
+
+    operator fun contains(d: MyDate): Boolean {
+        return start < d && d <= endInclusive
+    }
+}
